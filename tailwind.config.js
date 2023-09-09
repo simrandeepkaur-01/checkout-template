1f3ec1915443379e588cf8cdb065040940d3c5e7
simrandeep kaur <simrandeepkaur0119@gmail.com>
2023-09-09 20:05:52 +0530

    inserted checkout.html

---
+++ tailwind.config.js
@@ -0,0 +1,32 @@
+/** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme');
+
+module.exports = {
+  content: ["./src/**/*.{html, js}"],
+  theme: {
+    fontFamily: {
+      'sans': ['Poppins', ...defaultTheme.fontFamily.sans],
+    },
+    screens: {
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1436px',
+    },
+    extend: {
+      fontFamily: {
+        'heading': ['Open Sans', ...defaultTheme.fontFamily.sans],
+        'jost': ['Jost', 'sans-serif'],
+      },
+      screens: {
+        'xs': { 'min': '320px', 'max': '639px' },
+      },
+      maxHeight: {
+        '98': '42.5rem',
+      },
+    },
+  },
+  plugins: [],
+}
+
