186f7a3eed4cb2599d55be720c96f9ca1646de42
simrandeep kaur <simrandeepkaur0119@gmail.com>
2024-03-04 12:08:02 +0530

    initial commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,22 +7,20 @@ module.exports = {
     fontFamily: {
       'sans': ['Poppins', ...defaultTheme.fontFamily.sans],
     },
-    screens: {
-      'sm': '640px',
-      'md': '768px',
-      'lg': '1024px',
-      'xl': '1280px',
-      '2xl': '1436px',
-    },
     extend: {
       fontFamily: {
         'heading': ['Open Sans', ...defaultTheme.fontFamily.sans],
         'jost': ['Jost', 'sans-serif'],
       },
       screens: {
-        'xs': '639px',
+        'xs': '320px',
       },
     },
+    container:{
+      screens: {
+        '2xl':'1436px'
+      }
+    }
   },
   plugins: [],
 }
